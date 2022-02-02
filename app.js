2d214433c325c028bda319783186651ab6cfa02b
shovity <shovity@hub_produciton_database>
2022-02-02 05:48:21 +0000

    init

---
+++ app.js
@@ -0,0 +1,67 @@
+const eroc = require('eroc')
+const path = require('path')
+const auth = require('basic-auth')
+const util = require('util')
+
+const browser = require('./browser')
+let b = null
+
+
+const app = eroc.createApplication((app) => {
+
+    app.use(async (req, res, next) => {
+        const user = auth(req)
+    
+        if (user === undefined || user['name'] !== 'shovity' || user['pass'] !== 'ytivohs') {
+            res.statusCode = 401
+            res.setHeader('WWW-Authenticate', 'Basic realm="root"')
+            res.end('Unauthorized')
+        } else {
+            next()
+        }
+    })
+
+    app.get('/', async (req, res, next) => {
+        return res.sendFile(path.join(__dirname, 'index.html'))
+    })
+
+    app.get('/new', async (req, res, next) => {
+
+        const page = await browser.instance.newPage()
+        await page.goto('https://google.com')
+        
+        await browser.screenshot()
+
+        return res.success()
+    })
+
+    app.get('/screenshot', async (req, res, next) => {
+        return res.success(await browser.screenshot())
+    })
+
+    app.get('/command', async (req, res, next) => {
+        const command = req.gp('command')
+
+        if (command[0] === '/') {
+            const action = command.split(' ')[0].slice(1)
+
+            
+            return res.success(action)
+        }
+
+        try {
+            const handle = eval(`async () => {return (${command})}`)
+            const result = await handle()
+
+            return res.success(util.inspect(result))
+        } catch (error) {
+            return res.success(error.message || 'unknow error')
+        }
+    })
+    
+})
+
+app.start()
+browser.init().then(() => {
+    b = browser.instance
+})
---
+++ browser.js
@@ -0,0 +1,35 @@
+const Event = require('events')
+const puppeteer = require('puppeteer')
+
+
+const browser = new Event()
+
+
+browser.screenshot = async () => {
+    const pages = await browser.instance.pages()
+
+    return Promise.all(pages.map((page, index) => {
+        return page.screenshot({ encoding: "base64" })
+    }))
+
+}
+
+browser.init = async () => {
+    browser.instance = await puppeteer.launch({
+        // headless: false,
+        // slowMo: 100,
+        args: [
+            '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+            '--no-sandbox',
+        ],
+    })
+
+    browser.emit('ready')
+
+    const pages = await browser.instance.pages()
+    pages[0].goto('https://google.com')
+}
+
+
+
+module.exports = browser
